72a4acd926110a2165af378cf09e97c087214d85
Rebecca-Hallas <rebeccajhallas@gmail.com>
2021-04-23 11:34:33 +1200

    fixed migrations table

--- server/db/migrations/20210422231333_animals.js
+++ server/db/migrations/20210422231333_animals.js
@@ -1,14 +1,17 @@
 exports.up = function (knex) {
-    return knex.schema.createTable('animals', table => {
-      table.increments('id')
-      table.string('name')
-      table.integer('continent_id').references('continents.id')
-      table.string('animal_type')
-      table.integer('cuteness_rating')
-    })
-  }
-  
-  exports.down = function (knex) {
-    return knex.schema.dropTable('animals')
-  }
-  
+  return knex.schema.createTable('animals', table => {
+    table.increments('id')
+    table.string('animal')
+    table.string('emoji')
+    table.string('description')
+    table.integer('continent')
+    table.string('image')
+    table.integer('cuteness_rating')
+    table.integer('danger_rating')
+    table.integer('can_jump_over_car')
+  })
+}
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('animals')
+}
