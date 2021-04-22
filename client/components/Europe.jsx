c9530ccca0e55d5aca6aa140108a70f914358852
Rebecca-Hallas <rebeccajhallas@gmail.com>
2021-04-22 18:51:58 +1200

    add home, info, animals, and continents components

--- client/components/Europe.jsx
+++ client/components/Europe.jsx
@@ -1,14 +1,13 @@
 import React from 'react'
 // import { connect } from 'react-redux'
 
-function App (props) {
+function Europe (props) {
   return (
     <>
-      <div className='app'>
-        <h1>Virtual Safari</h1>
+      <div>
       </div>
     </>
   )
 }
 
-export default App
+export default Europe
