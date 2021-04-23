7636099d15ae6e720045f6c8139b671914608090
Sam Thorn <spthornnz@gmail.com>
2021-04-23 12:28:16 +1200

    Creates components for Landing Page

---
+++ client/components/HeroImage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+// import { connect } from 'react-redux'
+
+function HeroImage (props) {
+  return (
+    <>
+      <div>
+      </div>
+    </>
+  )
+}
+
+export default HeroImage
