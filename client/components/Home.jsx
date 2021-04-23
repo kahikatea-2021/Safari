43624015f569a3fbb1e9a4cbfdd73ce14a2cda61
1Karol1 <kbajczyk@gmail.com>
2021-04-23 15:37:43 +1200

    finaldesign

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -10,21 +10,18 @@ function Home (props) {
   return (
     <>
       <div className="background">
-        <h1>Virtual Safari</h1>
-        <div className="grid-container">Safari</div>
-        <div className="grid-item">  {props.continents.map(continent => (
-            <button className="buttonMain" key={continent}><Link to={`/${continent}`}>{continent}</Link></button>
+      
+        
+        <div className="grid-item"> <h1>Virtual Safari</h1>  {props.continents.map(continent => (
+            <div className="buttonMain" key={continent}><Link to={`/${continent}`}>{continent}</Link></div>
           ))}
         </div>
-        <p>Unvortunetely during COVID times our travel optons are very limited.
+        <p>Unvortunetely during COVID times  dsgfdfgdsf our travel optons are very limited.
           We found solution to it! Please travel with us on our Virtual Safari.
           You will get to see lots of interesting places and animals.
           Enjoy your journey! </p>
         <div><img className="car"src="./safari2.png"/></div>
-<<<<<<< HEAD
         
-=======
->>>>>>> 884b86ad70261186b01d496cd8d3dc75b64e1f39
       </div>
     </>
   )
