884b86ad70261186b01d496cd8d3dc75b64e1f39
johannbesas <johannbesas@gmail.com>
2021-04-23 14:57:18 +1200

    copied styles over

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,43 +1,33 @@
+
 import React, { useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
-import { fetchContinents, fetchAnimals } from '../actions'
-
-function Home(props) {
+import { fetchContinents } from '../actions'
+function Home (props) {
   useEffect(() => {
     props.dispatch(fetchContinents())
-    props.dispatch(fetchAnimals())
   }, [])
-
   return (
     <>
       <div className="background">
         <h1>Virtual Safari</h1>
-        
         <div className="grid-container">Safari</div>
-        <div className="grid-item">  
-        <ul>{props.continents.map(continent => (
+        <div className="grid-item">  {props.continents.map(continent => (
             <button className="buttonMain" key={continent}><Link to={`/${continent}`}>{continent}</Link></button>
           ))}
-          </ul>
-
         </div>
         <p>Unvortunetely during COVID times our travel optons are very limited.
           We found solution to it! Please travel with us on our Virtual Safari.
           You will get to see lots of interesting places and animals.
           Enjoy your journey! </p>
-        <div ><img className="car"src="./safari2.png"/></div>
-        
+        <div><img className="car"src="./safari2.png"/></div>
       </div>
     </>
   )
 }
-
 const mapStateToProps = (globalState) => {
   return {
-    continents: globalState.continents,
-    animals: globalState.animals
+    continents: globalState.continents
   }
 }
-
 export default connect(mapStateToProps)(Home)
