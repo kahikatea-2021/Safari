119fbf1660f7c782b4c7a5c9a8c7f49bc2bd3b21
Sam Thorn <spthornnz@gmail.com>
2021-04-24 13:24:45 +1200

    Page styling

--- client/components/AnimalInfo.jsx
+++ client/components/AnimalInfo.jsx
@@ -1,41 +1,36 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { Link, useParams } from 'react-router-dom'
 
 import { fetchAnimals } from '../actions'
 
+function Animal (props) {
+  useEffect(() => {
+    props.dispatch(fetchAnimals())
+  }, [])
 
+  return (
+    <>
+      {props.animals.filter(animal => animal.animal === useParams().name).map(animal =>
+        <div><h1>{animal.animal}</h1>
+          <p>Emoji: {animal.emoji}</p>
 
-function Animal(props) {
-    
-    useEffect(() => {
-      props.dispatch(fetchAnimals())
-    }, [])
-  
-  
-    return (
-      <>
-          {props.animals.filter(animal => animal.animal === useParams().name).map(animal => 
-          <div><h1>{animal.animal}</h1>
-            <p>Emoji: {animal.emoji}</p>
-          
-            <p>Rebecca's Description: {animal.description}</p>
-            <p>Continent: {animal.continent}</p>
-            <img src={`/images/${animal.image}`}/>
-            <p>Cuteness Rating: {animal.cuteness_rating}</p>
-            <p>Danger Rating: {animal.danger_rating}</p>
-            <p>{animal.can_jump_over_car}</p>
-          </div>
-          )}
-      </>
-    )
+          <p>Rebecca's Description: {animal.description}</p>
+          <p>Continent: {animal.continent}</p>
+          <img src={`/images/${animal.image}`}/>
+          <p>Cuteness Rating: {animal.cuteness_rating}</p>
+          <p>Danger Rating: {animal.danger_rating}</p>
+          <p>{animal.can_jump_over_car}</p>
+        </div>
+      )}
+    </>
+  )
+}
+
+const mapStateToProps = (globalState) => {
+  return {
+    animals: globalState.animals
   }
-  
-  const mapStateToProps = (globalState) => {
-    return {
-      animals: globalState.animals
-    }
-  }
-  
-  export default connect(mapStateToProps)(Animal)
-  
+}
+
+export default connect(mapStateToProps)(Animal)
